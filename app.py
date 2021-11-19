0cfde382a1b0728ec412908c3e6c53489ec0bc08
jlangner15 <langnerj@mcmaster.ca>
2021-11-19 16:33:07 -0500

    Make page for string

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    #render_template() automatically searches within templates directory for html page
+    return render_template('index.html')
+
+@app.route('/string/')
+def string():
+    return render_template('string.html')
 
 
 if __name__ == '__main__':
