3d6d264cfc92b0019cb9c9ca1d6f756e19b7430a
VaibhavMalik4187 <vaibhavmalik2019@gmail.com>
2021-11-21 01:01:40 +0530

    Added Code For Brackets_Validator_From_File

--- Brackets_Validator.py
+++ Brackets_Validator.py
@@ -2,6 +2,7 @@ from Stack import Stack
 from Brackets import Brackets
 
 def brackets_validator(str):
+    print(str)
     flag = True
     opening_bracket_stack = Stack()
 
@@ -25,6 +26,43 @@ def brackets_validator(str):
                 break
             else :
                 opening_bracket_stack.pop()
+
+    if (opening_bracket_stack.empty() and flag):
+        #print("Success")
+        return -1
+    else :
+        #print(opening_bracket_stack.top().posi)
+        return opening_bracket_stack.top().posi
+
+def brackets_validator_from_file(file_name):
+    opening_bracket_stack = Stack()
+    flag = True
+    position = 0
+
+    with open(file_name) as file:
+        while(true):
+            position += 1
+            ch = file.read(1)
+            if not char:
+                break
+
+            if (ch == '(' or ch == '[' or ch == '{'):
+                temp = Brackets(ch, position)
+                opening_bracket_stack.push(temp)
+
+            if (ch == ')' or ch == ']' or ch == '}'):
+                if opening_bracket_stack.empty():
+                    flag = False
+                    temp = Brackets(ch, position)
+                    opening_bracket_stack.push(temp)
+                    break
+                elif (not opening_bracket_stack.top().Match(ch)):
+                    flag = False
+                    temp = Brackets(ch, position)
+                    opening_bracket_stack.push(temp)
+                    break
+                else :
+                    opening_bracket_stack.pop()
 
     if (opening_bracket_stack.empty() and flag):
         #print("Success")
