18f729cfa3bbdc887ce96ad49d7456f13fabe7ac
jlangner15 <langnerj@mcmaster.ca>
2021-11-21 04:48:27 -0500

    One last fix, supported input types

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///strings.db'
 app.config['UPLOAD_FOLDER'] = 'static/' #folder to save files
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 #1MB max upload size
-app.config['UPLOAD_EXTENSIONS'] = ['.txt', '.py', '.doc', '.js', '.css', '.html', '.php', '.java', '.c'] #supported file extensions
+app.config['UPLOAD_EXTENSIONS'] = ['.txt', '.py', '.cpp', '.js', '.css', '.html', '.yaml', '.json', '.c'] #supported file extensions
 
 #init the database
 database = SQLAlchemy(app)
